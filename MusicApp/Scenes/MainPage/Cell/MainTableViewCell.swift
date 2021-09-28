b8c4a1621ee271d86f7bac8c985d5821af9d25a0
Semih Özsoy <semih__ozsoy@hotmail.com>
2021-09-28 18:32:35 +0300

    added main TableViewCell and xib file

---
+++ MusicApp/Scenes/MainPage/Cell/MainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.swift
+//  MusicApp
+//
+//  Created by mobven semih on 2.09.2021.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
