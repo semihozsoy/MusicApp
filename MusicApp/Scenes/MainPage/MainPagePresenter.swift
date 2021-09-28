11b71aa444191715fe5bf4c35708ec9b452b5dd4
Semih Özsoy <semih__ozsoy@hotmail.com>
2021-09-28 18:28:04 +0300

    fixed for topCharts and added for search

--- MusicApp/Scenes/MainPage/MainPagePresenter.swift
+++ MusicApp/Scenes/MainPage/MainPagePresenter.swift
@@ -3,13 +3,13 @@
 //  MusicApp
 //
 //  Created by Semih Özsoy on 18.08.2021.
-//
+// Copyright © 2021 Semih Özsoy. All rights reserved.
 
 import Foundation
 
 protocol MainPagePresentationLogic: AnyObject {
     func presentPlaylists(response: MainPage.FetchMusics.Response)
-    func presentCatalogPlaylists(response: MainPage.FetchMusics.Response)
+    func presentTopCharts(response: MainPage.FetchMusics.Response)
 }
 
 final class MainPagePresenter: MainPagePresentationLogic {
@@ -17,19 +17,33 @@ final class MainPagePresenter: MainPagePresentationLogic {
     weak var viewController: MainPageDisplayLogic?
     
     func presentPlaylists(response: MainPage.FetchMusics.Response) {
-        var displayedPlaylists: [MainPage.FetchMusics.ViewModel.PlaylistsInfo] = []
+        var displayedCatalogPlaylists: [MainPage.FetchMusics.ViewModel.CatalogPlaylistsInfo] = []
+        var displayedSearch: [MainPage.FetchMusics.ViewModel.SearchInfo] = []
+
+        response.catalogPlaylists?.forEach{
+            displayedCatalogPlaylists.append(MainPage.FetchMusics.ViewModel.CatalogPlaylistsInfo(albumName: $0.attributes?.name, imageUrl: $0.attributes?.artwork?.url,songCount: $0.relationships?.tracks?.data?.count))
+        }
         
-        
-        //        response.playlists?.forEach {
-        //            displayedPlaylists.append(MainPage.FetchMusics.ViewModel.PlaylistsInfo(name: $0.name,
-        //                                                                                   dateAdded: $0.dateAdded,
-        //                                                                                   isPublic: $0.isPublic))
-        //        }
-        //        viewController?.displayFetchedPlaylists(viewModel: MainPage.FetchMusics.ViewModel(playlistsInfo: displayedPlaylists))
+        for item in response.search ?? [] {
+            for searchItem in item.albums?.data ?? [] {
+                displayedSearch.append(MainPage.FetchMusics.ViewModel.SearchInfo(artistName: searchItem.attributes?.artistName , name: searchItem.attributes?.name))
+            }
+        }
+    
+        viewController?.displayFetchedPlaylists(viewModel: MainPage.FetchMusics.ViewModel(catalogPlaylistInfo: displayedCatalogPlaylists,searchInfo: displayedSearch))
         
     }
     
-    func presentCatalogPlaylists(response: MainPage.FetchMusics.Response) {
-        var displayedCatalogPlaylists: [MainPage.FetchMusics.ViewModel.CatalogPlaylistsInfo] = []
+    func presentTopCharts(response: MainPage.FetchMusics.Response) {
+        var displayedTopCharts: [MainPage.FetchMusics.ViewModel.TopChartsInfo] = []
+        for item in response.topCharts?.first?.songs?.first?.data ?? [] {
+            displayedTopCharts.append(MainPage.FetchMusics.ViewModel.TopChartsInfo(name: item.attributes?.name, image: item.attributes?.artwork?.url, playlistName: item.attributes?.artistName))
+        }
+        
+        viewController?.displayTopCharts(viewModel: MainPage.FetchMusics.ViewModel(topChartsInfo: displayedTopCharts))
     }
+    
 }
+
+
+
