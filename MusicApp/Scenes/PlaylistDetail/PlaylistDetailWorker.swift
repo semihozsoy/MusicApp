5eecde8e700083861c90047d41dcf160c9825432
Semih Özsoy <semih__ozsoy@hotmail.com>
2021-09-28 18:23:25 +0300

    added playlistDetail worker

---
+++ MusicApp/Scenes/PlaylistDetail/PlaylistDetailWorker.swift
@@ -0,0 +1,14 @@
+//
+//  PlaylistDetailWorker.swift
+//  MusicApp
+//
+//  Created by mobven semih on 1.09.2021.
+//
+
+import Foundation
+
+protocol PlaylistDetailWorkingLogic: AnyObject {
+}
+
+final class PlaylistDetailWorker: PlaylistDetailWorkingLogic {
+}
