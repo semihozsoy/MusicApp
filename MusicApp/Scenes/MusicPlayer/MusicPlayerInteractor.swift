6dac1288b6554db34954cd5e57d47ac7b9f52254
Semih Özsoy <semih__ozsoy@hotmail.com>
2021-09-28 18:36:50 +0300

    added musicPlayer interactor

---
+++ MusicApp/Scenes/MusicPlayer/MusicPlayerInteractor.swift
@@ -0,0 +1,48 @@
+//
+//  MusicPlayerInteractor.swift
+//  MusicApp
+//
+//  Created by mobven semih on 4.09.2021.
+//
+
+import Foundation
+
+protocol MusicPlayerBusinessLogic: AnyObject {
+    func fetchMusic(request: MusicPlayer.FetchMusic.Request)
+    func changeMusic(index: Int)
+    
+}
+
+protocol MusicPlayerDataStore: AnyObject {
+    var catalogPlaylist: [CatalogPlaylistsCuratorInfo] {get set}
+    var selectedIndex: Int {get set}
+}
+
+final class MusicPlayerInteractor: MusicPlayerBusinessLogic, MusicPlayerDataStore {
+    var catalogPlaylist: [CatalogPlaylistsCuratorInfo] = []
+    var presenter: MusicPlayerPresentationLogic?
+    var worker: MusicPlayerWorkingLogic?
+    var selectedIndex: Int = 0
+
+    
+    init(worker: MusicPlayerWorkingLogic) {
+        self.worker = worker
+    }
+
+    func fetchMusic(request: MusicPlayer.FetchMusic.Request) {
+        
+        selectedIndex = request.index == nil ? selectedIndex : request.index ?? 0
+    
+        guard catalogPlaylist.count > selectedIndex else { return }
+        let item = catalogPlaylist[safe:selectedIndex]
+       
+        self.presenter?.presentMusicPlayer(response: MusicPlayer.FetchMusic.Response(songName: item?.attributes?.name, playlistName: item?.attributes?.albumName ,image: item?.attributes?.artwork?.url, songUrl: item?.attributes?.previews?.first?.url))
+    }
+    
+    func changeMusic(index: Int) {
+        selectedIndex += index
+        fetchMusic(request: MusicPlayer.FetchMusic.Request(index: selectedIndex))
+    }
+    
+    
+}
