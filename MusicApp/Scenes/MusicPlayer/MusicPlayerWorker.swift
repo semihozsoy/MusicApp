067ee3ccc086ed187de856b57f386fbe071b189f
Semih Özsoy <semih__ozsoy@hotmail.com>
2021-09-28 18:39:29 +0300

    added musicPlayer worker

---
+++ MusicApp/Scenes/MusicPlayer/MusicPlayerWorker.swift
@@ -0,0 +1,16 @@
+//
+//  MusicPlayerWorker.swift
+//  MusicApp
+//
+//  Created by mobven semih on 4.09.2021.
+//
+
+import Foundation
+
+protocol MusicPlayerWorkingLogic: AnyObject {
+    
+}
+
+final class MusicPlayerWorker: MusicPlayerWorkingLogic {
+    
+}
