310471816499cb03d4805c05e4ee062b98faddca
Semih Özsoy <semih__ozsoy@hotmail.com>
2021-09-28 17:12:59 +0300

    outOfIndexRange extension included

---
+++ MusicApp/Extensions/OutOfIndexRange+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  OutOfIndexRange+Extension.swift
+//  MusicApp
+//
+//  Created by mobven semih on 23.09.2021.
+//
+
+import Foundation
+
+
+public extension Collection where Indices.Iterator.Element == Index {
+  /// Returns the element at the specified index iff it is within bounds, otherwise nil.
+  subscript (safe index: Index) -> Iterator.Element? {
+    return indices.contains(index) ? self[index] : nil
+  }
+}
