3e764fb2fda1697ec5857411ca9a686a88bbbcc6
Semih Özsoy <semih__ozsoy@hotmail.com>
2021-09-28 18:29:24 +0300

    change and edit MainPage Worker

--- MusicApp/Scenes/MainPage/MainPageWorker.swift
+++ MusicApp/Scenes/MainPage/MainPageWorker.swift
@@ -9,8 +9,9 @@ import Foundation
 
 protocol MainPageWorkingLogic: AnyObject {
     func fetchPlaylists(completion:@escaping ((Result<PlaylistsResponse,Error>)->Void))
-    func fetchCatalogPlaylists(id:String,completion:@escaping ((Result<CatalogPlaylistsCuratorInfo,Error>)->Void))
-    func fetchSearch(request: SearchPurpleAttributes.Request , completion: @escaping ((Result<Playlists,Error>)->Void))
+    func fetchCatalogPlaylists(id:String,completion:@escaping ((Result<CatalogPlaylists,Error>)->Void))
+    func fetchTopChart(completion: @escaping ((Result< TopCharts,Error>) -> Void))
+    func fetchSearch(request: SearchPurpleAttributes.Request , completion: @escaping ((Result<Search,Error>)->Void))
 }
 
 final class MainPageWorker: MainPageWorkingLogic {
@@ -21,11 +22,17 @@ final class MainPageWorker: MainPageWorkingLogic {
         networkManager.request(endpoint: .playlists, type: PlaylistsResponse.self, completion: completion)
     }
     
-    func fetchCatalogPlaylists(id: String, completion: @escaping ((Result<CatalogPlaylistsCuratorInfo, Error>) -> Void)) {
-        networkManager.request(endpoint: .catalogPlaylists(id: id), type: CatalogPlaylistsCuratorInfo.self, completion: completion)
+    func fetchTopChart(completion: @escaping ((Result<TopCharts, Error>) -> Void)) {
+        networkManager.request(endpoint: .topCharts, type: TopCharts.self, completion: completion)
     }
-    func fetchSearch(request: SearchPurpleAttributes.Request, completion: @escaping ((Result<Playlists, Error>) -> Void)) {
-        
+    
+    func fetchCatalogPlaylists(id: String, completion: @escaping ((Result<CatalogPlaylists, Error>) -> Void)) {
+        networkManager.request(endpoint: .catalogPlaylists(id: id), type: CatalogPlaylists.self, completion: completion)
     }
+    func fetchSearch(request: SearchPurpleAttributes.Request, completion: @escaping ((Result<Search, Error>) -> Void)) {
+        networkManager.request(endpoint: .search(queryParams: request.term ?? ""), type: Search.self, completion: completion)
+    }
+    
+
     
 }
