986717accb172a0683571046fdda923a7550d33b
Semih Özsoy <semih__ozsoy@hotmail.com>
2021-09-28 18:28:32 +0300

    added mainPage Router

--- MusicApp/Scenes/MainPage/MainPageRouter.swift
+++ MusicApp/Scenes/MainPage/MainPageRouter.swift
@@ -5,9 +5,10 @@
 //  Created by Semih Özsoy on 18.08.2021.
 //
 
-import Foundation
+import UIKit
 
 protocol MainPageRoutingLogic: AnyObject {
+    func routeToPlaylistDetail(index: Int)
 }
 
 protocol MainPageDataPassing: AnyObject {
@@ -17,4 +18,12 @@ protocol MainPageDataPassing: AnyObject {
 final class MainPageRouter: MainPageRoutingLogic, MainPageDataPassing {
     weak var viewController: MainPageViewController?
     var dataStore: MainPageDataStore?
+    
+    func routeToPlaylistDetail(index: Int) {
+        let storyBoard = UIStoryboard(name: "PlaylistDetail", bundle: nil)
+        let destinationVC = storyBoard.instantiateViewController(withIdentifier: "PlaylistDetailVC") as! PlaylistDetailViewController
+        destinationVC.router?.dataStore?.catalogPlaylist = dataStore?.catalogPlaylists?[index]
+        self.viewController?.navigationController?.pushViewController(destinationVC, animated: true)
+    }
 }
+
